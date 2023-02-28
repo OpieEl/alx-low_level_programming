aed2f4a2b2572860fbb016ee4379bb384c509d75
Opeyemi Elijah <opelijah20@gmail.com>
2023-02-28 07:02:08 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Check the code
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
